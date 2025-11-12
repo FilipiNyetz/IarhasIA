c85fc8d6c080fee977785bff6f7415081773ecfe
Joao Pedro <joaopedreleonel@gmail.com>
2025-11-12 01:49:52 -0300

    navigationStack

--- TesteGemni/MainTabView.swift
+++ TesteGemni/MainTabView.swift
@@ -8,41 +8,46 @@
 import SwiftUI
 
 struct MainTabView: View {
-
+    
     @StateObject var discViewModel = DiscViewModel()
     @StateObject var chatVm = ChatViewModel(manager: ManagerChat())
     @StateObject private var appState = AppState()
     
     var body: some View {
-
+        
         TabView(selection: $appState.selectedTab) {
-            // 🎯 Envolve cada aba em seu próprio NavigationStack para isolamento
-                        NavigationStack {
-                            HomeView()
-                        }
-                        .tabItem { Label("Início", systemImage: "house") }
-                        .tag(0)
-                        
-                        NavigationStack {
-                            // Passa o VM compartilhado
-                            ChatEntrevista(chatVm: chatVm)
-                        }
-                        .tabItem { Label("Iarhas IA", systemImage: "message.fill") }
-                        .tag(1) // A aba de destino é 1
-            ConexoesView()
-                .tabItem {
-                    Label("Conexões", systemImage: "person.3")
-                }
-            ProfileView()
-                .environmentObject(discViewModel)
-                .tabItem {
-                    Label("Perfil", systemImage: "person")
-                }
+            NavigationStack {
+                HomeView()
+            }
+            .tabItem { Label("Início", systemImage: "house") }
+            .tag(0)
+            
+            NavigationStack {
+                // Passa o VM compartilhado
+                ChatEntrevista(chatVm: chatVm)
+            }
+            .tabItem { Label("Iarhas IA", systemImage: "message.fill") }
+            .tag(1) // A aba de destino é 1
+            NavigationStack{
+                ConexoesView()
+            }
+            .tabItem {
+                Label("Conexões", systemImage: "person.3")
+            }
+            .tag(2)
+            NavigationStack{
+                ProfileView()
+            }
+            .environmentObject(discViewModel)
+            .tabItem {
+                Label("Perfil", systemImage: "person")
+            }
+            .tag(3)
         }
         .environmentObject(appState)
         .environmentObject(chatVm)
     }
-
+    
 }
 
 #Preview {
